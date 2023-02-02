81f9627ae02fea43401a030af73b43af12d25e7f
Emdadul <emdadul5023@onnorokom.com>
2023-02-02 17:19:17 +0600

    combination sum done

--- combinationSum.js
+++ combinationSum.js
@@ -1,42 +1,19 @@
 // https://takeuforward.org/interviews/strivers-sde-sheet-top-coding-interview-problems/
-let arr = [2, 1, 3]
-const target = 7
+let arr = [2, 3, 6, 7]
 let subArr = []
 
-// const cSum = (arr, indx, tempArr, sum) => {
-//     if(indx == arr.length) {
-//         const r = target-sum
-//         console.log('***'+sum + '\t' + r + '\t'+ [...tempArr])
-//         if(sum <= target && arr.includes(r)) {
-//             console.log(sum + '\t' + r)
-//             subArr.push([...tempArr, r])
-//         }
-//         return
-//     }
-
-//     cSum(arr, indx+1, [...tempArr, arr[indx]], sum+arr[indx])
-//     cSum(arr, indx+1, [...tempArr], sum)
-// }
-
-// cSum(arr, 0, [], 0)
-
-// console.log(subArr)
-
 const cSum = (arr, indx, target, tempArr) => {
-    if(indx == arr.length || target == 0) {
+    if(indx <= arr.length && target == 0) {
         subArr.push(tempArr)
         return
     }
-    if(arr[indx] > target) {
+    if(arr[indx] > target || indx == arr.length) {
         return
     }
     
-    
-    console.log(indx+'\t'+arr[indx]+'\t'+target)
-    console.log(tempArr)
     cSum(arr, indx, target-arr[indx], [...tempArr, arr[indx]])
     cSum(arr, indx+1, target, tempArr)
 }
 
-cSum(arr, 0, 7, [])
+cSum(arr, 0, 11, [])
 console.log(subArr)
